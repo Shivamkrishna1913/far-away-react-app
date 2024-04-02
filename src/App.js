9c05542864c22ec8f72fa36952f04358ff6e2c43
Shivam maurya <shivamaurya191002@gmail.com>
2024-04-02 21:58:48 +0530

    stats portion done

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
         onDeleteitem={handleDeleteitem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </>
   );
 }
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-export const Stats = () => {
+export const Stats = ({ items }) => {
+  const itemSize = items.length;
+  const packedItem = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedItem / itemSize) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list,and you slready packed X </em>
+      {!(percentage === 100) ? (
+        <em>
+          You have {itemSize} items on your list,and you slready packed{" "}
+          {packedItem} ,({percentage}
+          {"%"})
+        </em>
+      ) : (
+        <em>You got everything , Ready to go ✈️</em>
+      )}
     </footer>
   );
 };
