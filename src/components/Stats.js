ee9d1c80fe4f4188ef4520e204657b79f3de3c70
Shivam maurya <shivamaurya191002@gmail.com>
2024-04-02 22:02:06 +0530

    stats improved!!

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 export const Stats = ({ items }) => {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list🛩️</em>
+      </p>
+    );
+  }
+
   const itemSize = items.length;
   const packedItem = items.filter((item) => item.packed).length;
   const percentage = Math.round((packedItem / itemSize) * 100);
